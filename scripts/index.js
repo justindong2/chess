1c1daa72e10b6c47edf3f7ead8ed340d5d2f6ac2
justindong2 <69542168+justindong2@users.noreply.github.com>
2020-08-13 19:49:24 -0700

    Check for game over

--- scripts/index.js
+++ scripts/index.js
@@ -24,13 +24,14 @@ function makeRandomMove() {
 	var index = Math.floor(Math.random() * moves.length);
 	console.log(index);
 	var move = moves[index];
-	//setTimeout(function() {board.move(convertVerbose(move))}, 250);
 	board.move(convertVerbose(move));
 	pgn = chess.pgn();
 	chess.move(move);
-	// pgn += move.san;
 	pgnContainer.innerHTML = '<p>' + pgn + '</p>';
 	
+	if (chess.game_over()) {
+		return;
+	}
 	setTimeout(makeRandomMove, 300);
 }
 
@@ -52,30 +53,3 @@ var pgn = '';
 var pgnContainer = document.getElementById('pgnContainer');
 
 makeRandomMove();
-// for (i=0; i<200; i++) {
-	// setTimeout(makeRandomMove, 300 * i);
-// }
-// var i=1; // ply counter
-// while (!chess.game_over()) {
-	// setTimeout(makeRandomMove, 300 * i);
-	// i++;
-// }
-
-// for (i=0; i<100; i++) {
-	// setTimeout(function() {
-		// var moves = getMoves();
-		// var move = moves[0];
-		// board.move(convertVerbose(move));
-		// pgn = chess.pgn();
-		// chess.move(move);
-		// pgnContainer.innerHTML = '<p>' + pgn + '</p>';
-		// }, 300*i);
-// }
-
-// pgnContainer.innerHTML = '<p>' + pgn + '</p>';
-// game.move('b4');
-// console.log(pgn.innerHTML);
-// document.getElementById('pgn').innerHTML = moves[0];
-// pgnContainer.innerHTML = '<p>' + moves + '</p>';
-// alert(moves[0]);
-
