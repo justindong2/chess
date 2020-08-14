01d564c2e103803a7e7a41426141fbf7908ab9d7
justindong2 <69542168+justindong2@users.noreply.github.com>
2020-08-13 18:58:49 -0700

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,60 @@
+// adds first move in list to pgn string
+function addMove(pgn) {
+	var moves = chess.moves();
+	if (moves.length > 0) {
+		pgn += moves[0];
+	}
+	return pgn;
+}
+
+
+function getMoves() {
+	// returns verbose legal moves
+	return chess.moves({verbose: true});
+}
+
+function convertVerbose(move) {
+	var from = move.from;
+	var to = move.to;
+	return from + '-' + to;
+}
+
+function makeMove() {
+	
+}
+
+var config = {
+	position: 'start',
+	draggable: true
+};
+
+// alert(config.position);
+
+// creates chess board
+var board = Chessboard('game', config);
+
+// random chess game
+var chess = new Chess();
+// var moves = chess.moves();
+var pgn = '';
+// console.log(moves);
+var pgnContainer = document.getElementById('pgnContainer');
+for (i=0; i<100; i++) {
+	setTimeout(function() {
+		var moves = getMoves();
+		var move = moves[0];
+		//setTimeout(function() {board.move(convertVerbose(move))}, 250);
+		board.move(convertVerbose(move));
+		chess.move(move);
+		pgn += move.san;
+		pgnContainer.innerHTML = '<p>' + pgn + '</p>';
+		}, 250*i);
+}
+
+// pgnContainer.innerHTML = '<p>' + pgn + '</p>';
+// game.move('b4');
+// console.log(pgn.innerHTML);
+// document.getElementById('pgn').innerHTML = moves[0];
+// pgnContainer.innerHTML = '<p>' + moves + '</p>';
+// alert(moves[0]);
+
